2a4b323991c4d0235a161966ce3b5679f8061ba8
bts <ehdrud1129@naver.com>
2021-03-08 03:23:36 +0900

    edit models.py

--- project_managers/app_equipments/admin.py
+++ project_managers/app_equipments/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Equipment
+from .models import User, Notebook, Desktop, Monitor, Keyboard, Mouse, ExternalStroage, NetworkEquipment, SoundEquipment, Accessory, Etc, Equipment
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Notebook)
+admin.site.register(Desktop)
+admin.site.register(Monitor)
+admin.site.register(Keyboard)
+admin.site.register(Mouse)
+admin.site.register(ExternalStroage)
+admin.site.register(NetworkEquipment)
+admin.site.register(SoundEquipment)
+admin.site.register(Accessory)
+admin.site.register(Etc)
 admin.site.register(Equipment)
--- project_managers/app_equipments/models.py
+++ project_managers/app_equipments/models.py
@@ -1,21 +1,188 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import DateField
+import datetime
+
+
+RANK_CHOICES = (
+    ('', '직급 선택'),
+    ('s3', 's3'),
+    ('s4', 's4'),
+    ('s5', 's5'),
+    ('s6', 's6'),
+    ('s7', 's7'),
+    ('s8', 's8'),
+    ('freelancer', 'freelancer'),
+    ('intern', 'intern'),
+    ('contract', 'contract'),
+)
+class User(models.Model):
+    name = models.CharField(max_length=20, default='')  # CharField should be indicated with max_length
+    rank = models.CharField(max_length=20, default='', choices=RANK_CHOICES)
+    seat = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+
+NOTEBOOK_BRAND_CHOICES = (
+    ('', '브랜드 선택'),
+    ('gram', 'gram'),
+    ('ultra', 'ultra'),
+    ('samsung_itech', 'samsung_itech'),
+    ('asus', 'asus'),
+    ('hansung', 'hansung'),
+)
+class Notebook(models.Model):
+    brand = models.CharField(max_length=20, default='', choices=NOTEBOOK_BRAND_CHOICES)
+    cpu = models.CharField(max_length=50, blank=True)
+    ram = models.CharField(max_length=50, blank=True)
+    drive = models.CharField(max_length=50, blank=True)
+    gpu = models.CharField(max_length=50, blank=True)
+    purchase_date = DateField(blank=True, default=datetime.date.today)
+
+    def __str__(self):
+        return self.brand
+
+
+DESKTOP_PURPOSE_CHOICES = (
+    ('', '용도 선택'),
+    ('deep_learning', 'deep_learning'),
+    ('offer_interface', 'offer_interface'),
+    ('run_client_app', 'run_client_app'),
+)
+class Desktop(models.Model):
+    purpose = models.CharField(max_length=20, default='', choices=DESKTOP_PURPOSE_CHOICES)
+    cpu = models.CharField(max_length=50, blank=True)
+    ram = models.CharField(max_length=50, blank=True)
+    drive = models.CharField(max_length=50, blank=True)
+    gpu = models.CharField(max_length=50, blank=True)
+    purchase_date = DateField(blank=True, default=datetime.date.today)
+
+    def __str__(self):
+        return self.purpose
+
+
+MONITOR_BRAND_CHOICES = (
+    ('', '브랜드 선택'),
+    ('viewsync', 'viewsync'),
+    ('lg', 'lg'),
+    ('samsung_itech', 'samsung_itech'),
+    ('samsung', 'samsung'),
+)
+class Monitor(models.Model):
+    brand = models.CharField(max_length=20, default='', choices=MONITOR_BRAND_CHOICES)
+    inch = models.IntegerField(blank=True, default=32)
+    purchase_date = DateField(blank=True, default=datetime.date.today)
+
+    def __str__(self):
+        return self.brand
+
+
+KEYBOARD_BRAND_CHOICES = (
+    ('', '브랜드 선택'),
+    ('logitech_old', 'logitech_old'),
+    ('logitech_new', 'logitech_new'),
+    ('mechanical', 'mechanical'),
+)
+class Keyboard(models.Model):
+    brand = models.CharField(max_length=20, default='', choices=KEYBOARD_BRAND_CHOICES)
+    purchase_date = DateField(blank=True, default=datetime.date.today)
+
+    def __str__(self):
+        return self.brand
+
+
+MOUSE_BRAND_CHOICES = (
+    ('', '브랜드 선택'),
+    ('logitech_old', 'logitech_old'),
+    ('logitech_new', 'logitech_new'),
+    ('gram', 'gram'),
+    ('ultra', 'ultra'),
+    ('samsung_itech', 'samsung_itech'),
+    ('micronics', 'micronics'),
+)
+class Mouse(models.Model):
+    brand = models.CharField(max_length=20, default='', choices=MOUSE_BRAND_CHOICES)
+    purchase_date = DateField(blank=True, default=datetime.date.today)
+
+    def __str__(self):
+        return self.brand
+
+
+EXTERNALSTORAGE_TYPE_CHOICES = (
+    ('', '종류 선택'),
+    ('nas', 'nas'),
+    ('docking_station', 'docking_station'),
+    ('external_hdd', 'external_hdd'),
+    ('external_ssd', 'external_ssd'),
+
+)
+class ExternalStroage(models.Model):
+    type = models.CharField(max_length=20, default='', choices=EXTERNALSTORAGE_TYPE_CHOICES)
+    brand = models.CharField(max_length=20, blank=True, default='')
+    purchase_date = DateField(blank=True, default=datetime.date.today)
+
+    def __str__(self):
+        return self.type
+
+
+NETWORKEQUIPMENT_TYPE_CHOICES = (
+    ('', '종류 선택'),
+    ('vpn_router', 'vpn_router'),
+    ('hub', 'hub'),
+)
+class NetworkEquipment(models.Model):
+    type = models.CharField(max_length=20, default='', choices=NETWORKEQUIPMENT_TYPE_CHOICES)
+    brand = models.CharField(max_length=20, blank=True, default='')
+    purchase_date = DateField(blank=True, default=datetime.date.today)
+
+    def __str__(self):
+        return self.type
+
+
+SOUNDEQUIPMENT_TYPE_CHOICES = (
+    ('', '종류 선택'),
+    ('microphone', 'microphone'),
+    ('speaker', 'speaker'),
+)
+class SoundEquipment(models.Model):
+    type = models.CharField(max_length=20, default='', choices=SOUNDEQUIPMENT_TYPE_CHOICES)
+    brand = models.CharField(max_length=20, blank=True, default='')
+    purchase_date = DateField(blank=True, default=datetime.date.today)
+
+    def __str__(self):
+        return self.type
+
+ACCESSORY_TYPE_CHOICES = (
+    ('', '종류 선택'),
+    ('notebook_bag', 'notebook_bag'),
+    ('hdmi', 'hdmi'),
+)
+class Accessory(models.Model):
+    type = models.CharField(max_length=20, default='', choices=ACCESSORY_TYPE_CHOICES)
+    brand = models.CharField(max_length=20, blank=True, default='')
+    purchase_date = DateField(blank=True, default=datetime.date.today)
+
+    def __str__(self):
+        return self.type
+
+
+ETC_TYPE_CHOICES = (
+    ('', '종류 선택'),
+    ('kiosk', 'kiosk'),
+)
+class Etc(models.Model):
+    type = models.CharField(max_length=20, default='', choices=ETC_TYPE_CHOICES)
+    brand = models.CharField(max_length=20, blank=True, default='')
+    purchase_date = DateField(blank=True, default=datetime.date.today)
+
+    def __str__(self):
+        return self.type
+
 
 class Equipment(models.Model):
-
-    RANK_CHOICES = (
-        ('','직급을 선택해주세요.'),
-        ('S3', 'S3'),
-        ('S4', 'S4'),
-        ('S5', 'S5'),
-        ('S6', 'S6'),
-        ('S7', 'S7'),
-        ('S8', 'S8'),
-        ('Freelancer', 'Freelancer'),
-        ('Intern', 'Intern'),
-        ('Contract', 'Contract'),
-    )
 
     MAINBODY_CHOICES = (
         ('', '노트북/데스크탑 선택'),
