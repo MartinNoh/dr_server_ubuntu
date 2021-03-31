e900db6fdc4f9d59d7d22becf333e1053416569d
MartinNoh <ehdrud1129@naver.com>
2021-04-01 01:18:09 +0900

    docker-compose 활용

--- project_managers/project_managers/settings.py
+++ project_managers/project_managers/settings.py
@@ -85,8 +85,17 @@ WSGI_APPLICATION = 'project_managers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'equipments',
+        #'USER': 'root',
+        #'PASSWORD': 'saltlux#20',
+        'USER': 'donggyeong',
+        'PASSWORD': 'shehdrud12',
+        #'HOST': 'localhost', # default: localhost
+        'HOST': '172.17.0.1', # mysql_service container
+        'PORT': '3306', # default: 3306
     }
 }
 
